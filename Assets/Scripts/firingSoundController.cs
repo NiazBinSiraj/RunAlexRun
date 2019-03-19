cb2cfa0fc880c24d7688e5341aef335c836f65ed
Jalal Uddin Md. Akbar <poroshgh@gmail.com>
2019-03-19 19:41:40 +0600

    Added Bullet Firing Sound Controller Script

---
+++ Assets/Scripts/firingSoundController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firingSoundController : MonoBehaviour
+{
+    private AudioSource firingSound;
+    public AudioClip firing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firingSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playFiringSound()
+    {
+        firingSound.PlayOneShot(firing, 0.3f);
+    }
+
+}
