61703a2d437c0d848aa2a2c780ae46ed9863c953
Jalal Uddin Md. Akbar <poroshgh@gmail.com>
2019-03-19 19:37:44 +0600

    Added Bullet Control Script

---
+++ Assets/Scripts/Bullet_Control.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Control : MonoBehaviour
+{
+
+    public Vector2 speed;
+    Rigidbody2D rb2d;
+    public float Damage, delay;
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(speed);
+        Destroy(gameObject, delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.AddForce(speed * 100);
+    }
+
+    void OnTriggerEnter2D(Collider2D Other)
+    {
+        if (Other.gameObject.CompareTag("Big Enemy") || Other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
